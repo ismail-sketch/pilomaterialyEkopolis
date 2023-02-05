950dc4c06d3117f55b20b504cb94ffa213b1ab42
Gennadiy <gennadiy.mutalibov@gmail.com>
2023-02-05 18:59:31 +0300

    еще коммит

--- js/index.js
+++ js/index.js
@@ -1,13 +1,17 @@
-const burger = document.querySelector('.burger');
-const burgerSpan = document.querySelectorAll('.burger span');
-const menuList = document.querySelector('.menu');
 const body = document.querySelector('body');
+const burger = document.querySelector('.burger');
+const headerContainer = document.querySelector('.header-container');
+const headerContacts = document.querySelector('.header__contacts');
+const burgerSpan = document.querySelectorAll('.burger span');
+const menu = document.querySelector('.menu');
+const menuList = document.querySelector('.menu__list');
+
 
 burger.addEventListener('click', () => {
     burgerSpan.forEach(item => {
         item.classList.toggle('active');
     })
-    menuList.classList.toggle('active');
+    menu.classList.toggle('active');
     body.classList.toggle('active');
 })
 
@@ -16,7 +20,22 @@ window.addEventListener('click', (e) => {
         burgerSpan.forEach(item => {
             item.classList.remove('active');
         })
-        menuList.classList.remove('active');
+        menu.classList.remove('active');
         body.classList.remove('active');
     }
 })
+
+// sub-menu=======================
+const subMenu = document.querySelector('.sub-menu');
+const clickMenu = document.querySelector('.click-menu');
+
+// Перемещение элементов в меню
+function moveMenuEl() {
+    if(window.innerWidth < 750) {
+        menuList.insertAdjacentElement('beforeend', headerContacts);
+    } else {
+        headerContainer.insertAdjacentElement('beforeend', headerContacts);
+    }
+}
+moveMenuEl();
+window.addEventListener('resize', moveMenuEl);
